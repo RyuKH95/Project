a294e0b5d304a33e6733913a96b3dd0bfd21fff4
RyuKH95 <gh9503@gmail.com>
2022-01-04 23:30:43 +0900

    Night Day

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // alert("Hello, world!");
 
 // console.log("Hello, world!");
+
+function Night() {
+    document.querySelector('body').style.backgroundColor='black';
+    document.querySelector('body').style.color='white';
+}
+function Day() {
+    document.querySelector('body').style.backgroundColor='white';
+    document.querySelector('body').style.color='black';
+}
