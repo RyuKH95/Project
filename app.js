198e964925fd9b010a99b4d2343fba70a6475075
RyuKH95 <gh9503@gmail.com>
2022-01-06 10:23:32 +0900

    function

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-// alert("Hello, world!");
-
-// console.log("Hello, world!");
-
-function Night() {
-    document.querySelector('body').style.backgroundColor='black';
-    document.querySelector('body').style.color='white';
+function reference() {
+    document.write('<ul>');
+    document.write('참고')
+    const referenceList = {}
+    referenceList['생활코딩']="https://opentutorials.org/course/1";
+    referenceList['노마드코더']="https://nomadcoders.co/";
+    referenceList['unsplash']="https://unsplash.com/";
+    for(const key in referenceList) {
+        document.write('<li>');
+        document.write(key, ' - <a href="', referenceList[key],'" target="_blank">', referenceList[key], '</a>');
+        document.write('</li>');
+    }
+    document.write('</ul');
 }
-function Day() {
-    document.querySelector('body').style.backgroundColor='white';
-    document.querySelector('body').style.color='black';
+
+function Tawk_API() {
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/61d28d92c82c976b71c47a83/1fof7i1cs';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+}
+
+// 3.html
+function Toggle(t) {
+    const target = document.querySelector('body').style;
+    const links = document.querySelectorAll('a');
+    if(t.value=='Day') {
+        target.backgroundColor='white';
+        target.color='black';
+        t.value='Night';
+        for(let i=0; i<links.length; i++) {
+            links[i].style.color='blue';
+        }
+    }
+    else {
+        target.backgroundColor='black';
+        target.color='white';
+        t.value='Day';
+        for(let i=0; i<links.length; i++) {
+            links[i].style.color='powderblue';
+        }
+    }
 }
