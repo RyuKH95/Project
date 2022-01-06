102c9bd4522a1d597bfc6f8c470cb39f3f805d6b
RyuKH95 <gh9503@gmail.com>
2022-01-06 22:33:13 +0900

    jquery

--- app.js
+++ app.js
@@ -35,18 +35,21 @@ function reference() {
 // 3.html
 const Body = {
     setColor: function(color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 const Links = {
     setColor: function(color) {
-        const links = document.querySelectorAll('a');
-        for(let i=0; i<links.length; i++) {
-            links[i].style.color=color;
-        }
+        // const links = document.querySelectorAll('a');
+        // for(let i=0; i<links.length; i++) {
+        //     links[i].style.color=color;
+        // }
+        $('a').css('color', color);
     }
 }
 function Toggle(self) {
