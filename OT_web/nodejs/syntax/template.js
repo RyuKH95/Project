80092cce68a6b2cf1820e72add8358d9c08d025e
RyuKH95 <gh9503@gmail.com>
2022-01-13 17:58:41 +0900

    template backtic

---
+++ OT_web/nodejs/syntax/template.js
@@ -0,0 +1,10 @@
+const name = 'egoing';
+const letter = 'Lorem '+name+'\n\
+\n\
+ ipsum dolor sit amet consectetur adipisicing elit. '+name+' At officia tempora velit enim harum voluptatibus dignissimos aliquid dolorem molestias, doloremque repellat. Sint, est eveniet? Eos nam';
+console.log(letter);
+
+const letter2 = `Lorem ${name}
+
+ ipsum dolor sit amet consectetur adipisicing elit. ${name} At officia tempora velit enim harum voluptatibus dignissimos aliquid dolorem molestias, doloremque repellat. Sint, est eveniet? Eos nam`;
+console.log(letter2);
