640c51a9023b12b4c0f5053e64bd68faf74aa2fa
RyuKH95 <gh9503@gmail.com>
2022-01-14 17:44:08 +0900

    readdir

--- OT_web/nodejs/main.js
+++ OT_web/nodejs/main.js
@@ -1,21 +1,17 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const app = http.createServer(function(request,response){
-    let _url = request.url;
-    const queryData = url.parse(_url, true).query;
-    let title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      // if(err) None;
-      const template = `
+const app = http.createServer(function (request, response) {
+  let _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+        const title = 'Welcome';
+        description = 'Hello, Node.js';
+        const template = `
       <!doctype html>
       <html>
       <head>
@@ -34,8 +30,37 @@ const app = http.createServer(function(request,response){
       </body>
       </html>
       `;
-      response.end(template);
-    });
- 
+        response.writeHead(200);
+        response.end(template);
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        let title = queryData.id;
+        const template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>${description}</p>
+      </body>
+      </html>
+      `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
