379e26d7c9310878c0e14da87b2eebf4c7a10d73
RyuKH95 <gh9503@gmail.com>
2022-01-17 10:54:19 +0900

    function

--- OT_web/nodejs/main.js
+++ OT_web/nodejs/main.js
@@ -2,6 +2,32 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+  </head>
+  <body>
+  <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+  </body>
+  </html>
+  `
+}
+
+function templateList(filelist) {
+  let list = `<ol>`;
+  for (let i = 0; i < filelist.length; i++) {
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list += `</ol>`;
+  return list;
+}
+
 const app = http.createServer(function (request, response) {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
@@ -12,57 +38,24 @@ const app = http.createServer(function (request, response) {
       fs.readdir("./data", function (err, filelist) {
         const title = "Welcome";
         const description = "Hello, Node.js";
-        let list = `<ol>`;
-        for (let i = 0; i < filelist.length; i++) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += `</ol>`;
+        
+        const list = templateList(filelist);
 
-        const template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        `;
+        const template = templateHTML(title, list, `<h2>${title}</h2>
+        <p>${description}</p>`);
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", function (err, filelist) {
-        let list = `<ol>`;
-        for (let i = 0; i < filelist.length; i++) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += `</ol>`;
         fs.readFile(
           `data/${queryData.id}`,
           "utf8",
           function (err, description) {
             let title = queryData.id;
-            const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}</p>
-      </body>
-      </html>
-      `;
+            const list = templateList(filelist);
+            const template = templateHTML(title, list, `<h2>${title}</h2>
+            <p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           }
---
+++ OT_web/nodejs/syntax/function2.js
@@ -0,0 +1,7 @@
+console.log(Math.max(10,3,4,21));
+
+console.log(sum(3,4));
+
+function sum(a, b) {
+    return a + b;
+}
